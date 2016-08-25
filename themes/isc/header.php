0a29249860fa81025e7f628582235d165321ad44
abrahamhinojo <abraham.hinojo@gmail.com>
2016-08-25 00:11:36 -0700

    icono de la pagina x-icon

--- themes/isc/header.php
+++ themes/isc/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
 <head profile="http://gmpg.org/xfn/11">
+	<link href="<?php bloginfo('template_url') ?>/assets/ico.png" type="image/x-icon" rel="shortcut icon">
 	<title><?php wp_title( '-', true, 'right' ); echo _wp_specialchars( get_bloginfo('name'), 1 ) ?></title>
 	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
 
